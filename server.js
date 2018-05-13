7a0aa41406665cd4adea6020633ba9b806fc6dc4
Mike Skalandunas <mskalandunas@gmail.com>
2018-05-12 22:19:46 -0700

    [server] rm server

--- server.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(express.static(`${__dirname}/dist`));
-app.listen(port, console.log(`Listening on ${port}.`)); // eslint-disable-line no-console
